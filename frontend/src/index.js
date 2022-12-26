bd5ab79eee831edd5dcee4f2f4f2ffe642d461c5
Haidar Yahfoufi <haidar.yahfoufi@gmail.com>
2022-12-25 23:55:09 -0500

    Setup react redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "./styles/icons/icons.css";
 import App from "./App";
-import {BrowserRouter as Router} from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
+const store = configureStore({ reducer: rootReducer });
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
-
