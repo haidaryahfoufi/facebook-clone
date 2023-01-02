c4365e2f0c7b79d0775490d1bc56a98d46f098b2
Haidar Yahfoufi <haidar.yahfoufi@gmail.com>
2023-01-01 22:15:28 -0500

    Finish the footer for the login page

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -75,6 +75,63 @@ export default function Login() {
           </div>
         </div>
         <div className="login_register"></div>
+        <div className="login_footer">
+          <div className="login_footer_wrap">
+            <Link to="/">English (US)</Link>
+            <Link to="/">Français (Canada)</Link>
+            <Link to="/">Español</Link>
+            <Link to="/">中文(简体)</Link>
+            <Link to="/">한국어</Link>
+            <Link to="/">日本語</Link>
+            <Link to="/">Português (Brasil)</Link>
+            <Link to="/">Deutsch</Link>
+            <Link to="/">Italiano</Link>
+            <Link to="/">العربية</Link>
+            <Link to="/">हिन्दी</Link>
+            <Link to="/" className="footer_square">
+              <i className="plus_icon"></i>
+            </Link>
+          </div>
+          <div className="footer_splitter"></div>
+          <div className="login_footer_wrap">
+            <Link to="/">Sign Up</Link>
+            <Link to="/">Log In</Link>
+            <Link to="/">Messenger</Link>
+            <Link to="/">Facebook Lite</Link>
+            <Link to="/">Watch</Link>
+            <Link to="/">Places</Link>
+            <Link to="/">Games</Link>
+            <Link to="/">Marketplace</Link>
+            <Link to="/">Meta Play</Link>
+            <Link to="/">Oculus</Link>
+            <Link to="/">Portal</Link>
+            <Link to="/">Instagram</Link>
+            <Link to="/">Bulletin</Link>
+            <Link to="/">Fundraisers</Link>
+            <Link to="/">Services</Link>
+            <Link to="/">Voting Information Center</Link>
+            <Link to="/">Privacy Policy</Link>
+            <Link to="/">Groups</Link>
+            <Link to="/">About</Link>
+            <Link to="/">Create Ad</Link>
+            <Link to="/">Create Page</Link>
+            <Link to="/">Developers</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Cookies</Link>
+            <Link to="/">
+              AdChoices
+              <i className="adChoices_icon"></i>
+            </Link>
+            <Link to="/">Terms</Link>
+            <Link to="/">Help</Link>
+            <Link to="/">Contact Uploading & Non-Users</Link>
+          </div>
+          <div className="login_footer_wrap">
+            <Link to="/" style={{fontsize: "12px", marginTop: "10px"}}>
+              Meta © 2023
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
